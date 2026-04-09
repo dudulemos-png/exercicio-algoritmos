49daa93471af6bb9af07fa829bd1cba5a1463e21
dudulemos-png <dudulemos2002@yahoo.com.br>
2026-04-09 00:04:55 -0300

    Add files via upload

---
+++ algoritmos1.c
@@ -0,0 +1,11 @@
+#include <stdio.h>
+int main()
+{
+    float numero;
+    printf("digite um numero: ");
+    scanf("%f", &numero);
+    float resultado;
+    resultado = numero / 2;
+    printf("a metade é: %f", resultado);
+    return 0;
+}
